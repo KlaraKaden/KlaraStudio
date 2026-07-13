10119085c6b2a7ad0e249bf7986575a2a9d238ff
Klara-the-sheep <klarakaden@web.de>
2026-07-13 22:11:22 +0200

    Fix Nuxt GitHub Pages baseURL and pnpm workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,18 @@ export default defineNuxtConfig({
   // css: [
   //   '~/assets/style.css',
   // ],
+
+  // Für GitHub Pages (statisches Hosting)
+  ssr: false,
+
+  app: {
+    // lokal "/" | auf GitHub Pages "/KlaraStudio/"
+    // baseURL: process.env.NODE_ENV === 'production' ? '/KlaraStudio/' : '/',
+    baseURL: '/KlaraStudio/',
+  },
+
+  // WICHTIG für Nuxt auf GitHub Pages
+  nitro: {
+    preset: 'github_pages',
+  },
 })
